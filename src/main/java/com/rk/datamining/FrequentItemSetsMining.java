3ecec474fca608a51b922d543707291c7b0acf99
Gayathri and Ramesh <tankbell@Gayathris-MacBook-Pro.local>
2019-03-16 11:03:35 -0700

    second commit

--- src/main/java/com/rk/datamining/FrequentItemSetsMining.java
+++ src/main/java/com/rk/datamining/FrequentItemSetsMining.java
@@ -1,140 +1,243 @@
 package com.rk.datamining;
 
-import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileReader;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.FileWriter;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
 import java.util.Set;
-
-import com.google.common.collect.ImmutableSet;
+import java.util.stream.Collectors;
 import com.google.common.collect.Sets;
 
-
+/**
+ * Frequent Item Sets Mining based on the
+ * Apriori Algorithm.
+ * @author Ram Kulathumani
+ *
+ */
 public class FrequentItemSetsMining {
-	Map<ImmutableSet<Integer>, Integer> itemSetsFrequency =
-			new HashMap<ImmutableSet<Integer>, Integer>();
-	// By default find tripletons of transactions if no user argument
-	// is specified.
-	int ITEMSETS_SIZE = 3;
-	// Default SIGMA Value if the user has not specified
-	int SIGMA = 3;
 	// Default File input and output
-	static File input = new File("src/test/resources/test.dat");
+	static String input = "src/test/resources/test.dat";
 	static File output = new File("src/test/resources/output.txt");
-	
+
+    // Find item sets of size in the range [3, ITEMSETS_SIZE]
+	// By default find item sets of size in the range [3,4]
+	static int ITEMSETS_SIZE = 4;
+
+	// Default SIGMA Value if the user has not specified
+	// The frequency of the item sets should be atleast
+	// equal to the value of SIGMA
+	static int SIGMA = 4;
+
+	// All transactions
+	List<Set<Integer>> allTransactions = new ArrayList<Set<Integer>>();
+
+	// A frequency table for itemsets.
+	// Only hold in memory the item sets required for the 
+	// current iteration.
+	// Gets written to the output file.
+	Map<Set<Integer>, Integer> itemSetsFrequencyTable = 
+			new HashMap<Set<Integer>, Integer>();
+
+	BufferedWriter bufferedWriter;
+
 	/**
-	 * Gets all combinations of a given size
-	 * @param i The immutable set for which we need
-	 *          to get all the combinations for.
-	 * @param k The size of the combinations.
-	 * @return  The combinations of the given size.
+	 * Create the transactions set
+	 * @param transaction The list of transactions
 	 */
-	Set<Set<Integer>> getCombinations(Set<Integer> i,
-			int k) {
-		if (i.size() < k) return null;
-		return Sets.combinations(i, k);
-	}
-	
-	/**
-	 * Updates the ItemSetFrequencyMap with the count of the
-	 * given ItemSet.
-	 * @param itemSet The immutable itemSet Key of the Map.
-	 */
-	void updateItemSetsFrequencyMap(ImmutableSet<Integer> itemSet) {
-		if (itemSetsFrequency.containsKey(itemSet)) {
-			int c = itemSetsFrequency.get(itemSet);
-			itemSetsFrequency.put(itemSet, c + 1);
-		} else {
-			itemSetsFrequency.put(itemSet, 1);
+	void createTransactions(List<String> transaction) {
+		Set<Integer> set = new HashSet<Integer>();
+		for (String item : transaction) {
+			set.add(Integer.valueOf(item));
 		}
+		allTransactions.add(set);
 	}
-	
+
 	/**
-	 * Prune the Frequency Map according to the given
-	 * Threshold value (SIGMA) 
+	 * Count the frequency of each unique SKU.
+	 * in parallel and compute the results in a
+	 * map reduce fashion
 	 */
-	void pruneFrequencyMap() {
-		Iterator<Entry<ImmutableSet<Integer>, Integer>> it = 
-				itemSetsFrequency.entrySet().iterator();
-		while (it.hasNext()) {
-			Map.Entry<ImmutableSet<Integer>, Integer> kv = 
-					(Map.Entry<ImmutableSet<Integer>, Integer>)it.next();
-			if (kv.getValue() < SIGMA) it.remove();
-		}
+	void countSKUFrequency() {
+		allTransactions.parallelStream().flatMap(Set::stream)
+				.collect(Collectors.toMap(sku -> sku, sku -> 1, Integer::sum)).forEach((k, v) -> {
+					if (v >= SIGMA)
+						itemSetsFrequencyTable.put(Set.of(k), v);
+				});
 	}
-	
-	void writeOutput() {
-		BufferedWriter bufferedWriter = null;
+
+	/**
+	 * Check the frequency of each item set in the 
+	 * transactions. Runs in parallel and computes the
+	 * result in a map reduce fashion
+	 * @param itemSet The ItemSet of SKUs
+	 */
+	void compareAndCountItemSetsFrequency(Set<Integer> itemSet) {
+		allTransactions.parallelStream().filter(item -> item.containsAll(itemSet))
+				.collect(Collectors.toMap(t -> itemSet, t -> 1, Integer::sum)).forEach((k, v) -> {
+					if (v >= SIGMA) {
+						itemSetsFrequencyTable.put(k, v);
+					}
+				});
+	}
+
+	/**
+	 * Read the transactions file.
+	 */
+	public void init() {
 		try {
-			bufferedWriter = 
-					new BufferedWriter(new FileWriter(output));
-			Iterator<Entry<ImmutableSet<Integer>, Integer>> it = 
-					itemSetsFrequency.entrySet().iterator();
-			while (it.hasNext()) {
-				Map.Entry<ImmutableSet<Integer>, Integer> kv = 
-						(Map.Entry<ImmutableSet<Integer>, Integer>)it.next();
-				bufferedWriter.write(String.valueOf(kv.getKey().size()));
+			Path filePath = Paths.get(input);
+			Files.lines(filePath).parallel().map(line -> line.split("\\s+")).map(item -> Arrays.asList(item))
+					.collect(Collectors.toSet()).forEach(item -> createTransactions(item));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Writes the Frequency Table to the output.
+	 */
+	void writeToOutput() {
+		try {
+			for (Set<Integer> key : itemSetsFrequencyTable.keySet()) {
+				bufferedWriter.write("ItemSet Size:" + String.valueOf(key.size()));
 				bufferedWriter.append(',');
-				bufferedWriter.append(kv.getValue().toString());
+				bufferedWriter.append("Frequency:" + itemSetsFrequencyTable.get(key).toString());
 				bufferedWriter.append(',');
-				bufferedWriter.append(kv.getKey().toString());
+				bufferedWriter.append("ItemSet:" + key.toString());
+				bufferedWriter.append("\n");
 			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Write the item sets of size K to a file
+	 * Creates file with extension .serialized
+	 * @param s The item set to be written
+	 * @param o The ObjectOutputStream
+	 */
+	synchronized void writeItemSetToFile(Set<Integer> s,
+			ObjectOutputStream o) {
+		try {
+			o.writeObject(s);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Builds the Item Sets before pruning based on
+	 * the value of SIGMA. Writes output to file to
+	 * save memory
+	 * @param prevItemSets Set of SKU Values
+	 * @param cardinality The dimension of the item set.
+	 */
+	void buildItemSets(Set<Set<Integer>> prevItemSets, int cardinality) {
+		try {
+			FileOutputStream f = 
+					new FileOutputStream(new File("000000" + cardinality + ".serialized"));
+			ObjectOutputStream o = new ObjectOutputStream(f);
+			prevItemSets.parallelStream().forEach(x -> {
+				prevItemSets.stream().forEach(y -> {
+					if (Sets.intersection(x, y).size() == cardinality - 2) {
+						Set<Integer> itemSet = new HashSet<Integer>();
+						itemSet.addAll(Sets.union(x, y));
+						writeItemSetToFile(itemSet, o);
+						itemSet = null;
+						// This isn't really necessary.
+						// Just trying everything possible to
+						// keep the heap size in check. Did
+						// help in the machine used for tests.
+						System.gc();
+					}
+				});
+			});
+			o.writeObject(null);
+			o.close();
+			f.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	void run() {
+		init();
+
+		countSKUFrequency();
+
+		// Initialize for K = 1 and iterate for the rest
+		int k = 1;
+
+		try {
+			bufferedWriter = new BufferedWriter(new FileWriter(output));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		while (!itemSetsFrequencyTable.keySet().isEmpty()) { 
+			writeToOutput();
+			// Reached the end , exit.
+			if (k == ITEMSETS_SIZE)
+				break;
+			k = k + 1;
+			// Generate item sets from the pruned list
+			// of the previous iteration
+			buildItemSets(itemSetsFrequencyTable.keySet(), k);
+			itemSetsFrequencyTable.clear();
+			System.gc();
+			FileInputStream f;
+			boolean b = true;
+			ObjectInputStream input;
+			try {
+				f = new FileInputStream("000000" + k + ".serialized");
+				input = new ObjectInputStream(f);
+				while (b) {
+					Object object = input.readObject();
+					if (object != null) {
+						Set<Integer> itemSet = (Set<Integer>) object;
+						compareAndCountItemSetsFrequency(itemSet);
+					} else {
+						b = false;
+					}
+				}
+				input.close();
+				f.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		try {
 			bufferedWriter.close();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
-	/**
-	 * Read input file. Also computes the frequency of
-	 * itemsets upto size n.
-	 * @param input The file containing
-	 *              the transaction.
-	 */
-	void run(File input) {
-		try {
-			BufferedReader bufferedReader = 
-					new BufferedReader(new FileReader(input));
-			String transaction;
-			while ((transaction = bufferedReader.readLine()) != null) {
-				String[] str = transaction.split("\\s+");
-				Set<Integer> items = new HashSet<Integer>();
-				for (int i = 0; i < str.length ; i++) {
-					if (str[i].isEmpty()) continue;
-					items.add(Integer.parseInt(str[i]));
-				}
-				Set<Set<Integer>> itemSets = 
-						getCombinations(items, ITEMSETS_SIZE);
-				if (itemSets == null) continue;
-				for (Set<Integer> ele : itemSets) {
-		    		ImmutableSet<Integer> combination = ImmutableSet.<Integer>builder()
-		    		           .addAll(ele)
-		    		           .build();
-		    		updateItemSetsFrequencyMap(combination);
-		    	}
-			}
-			bufferedReader.close();
-			pruneFrequencyMap();
-			writeOutput();
-		} catch (Exception e) {
-			e.printStackTrace();
+
+	public static void main(String[] args) {
+		FrequentItemSetsMining mining = new FrequentItemSetsMining();
+		if (args.length == 1) {
+			input = args[0];
+		} else if (args.length == 2) {
+			input = args[0];
+			SIGMA = Integer.valueOf(args[1]);
+		} else if (args.length == 3) {
+			input = args[0];
+			SIGMA = Integer.valueOf(args[1]);
+			ITEMSETS_SIZE = Integer.valueOf(args[2]);
 		}
+		
+		mining.run();
 	}
-	
-    public static void main(String[] args) {
-        FrequentItemSetsMining mining = new FrequentItemSetsMining();
-        if (0 < args.length) {
-            input = new File(args[0]);
-        }
-        long st = System.currentTimeMillis();
-        mining.run(input);
-        long et = System.currentTimeMillis() - st;
-        System.out.println(et);
-    }
 }
--- src/test/java/com/rk/datamining/FrequentItemSetsMiningTest.java
+++ src/test/java/com/rk/datamining/FrequentItemSetsMiningTest.java
@@ -13,119 +13,26 @@ import static org.junit.Assert.assertNotNull;
 
 import java.util.List;
 import java.io.File;
+import java.io.IOException;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.TreeSet;
+import org.apache.commons.io.FileUtils;
 
 public class FrequentItemSetsMiningTest {
-	/*
 	@Test
 	public void testAlgorithm() {
 		FrequentItemSetsMining mining = new FrequentItemSetsMining();
-		mining.run(new File("src/test/resources/test.dat"));
-		assertEquals("Incorrect Frequent Sets Map Size", mining.itemSetsFrequency.size(), 1);
-		ImmutableSet<Integer> r = ImmutableSet.of(80,1001,1003);
-		assertNotNull("Missing valid Frequent Set Map Key", mining.itemSetsFrequency.get(r));
-		int x = mining.itemSetsFrequency.get(r);
-		assertEquals("Incorrect Frequent Set Map Value", x, 3);
+		mining.run();
+		boolean resultOk = false;
+		File op = new File("src/test/resources/output.txt");
+		File exp = new File("src/test/resources/expected.txt");
+		try {
+			resultOk = FileUtils.contentEquals(op, exp);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		assertEquals("Incorrect results", true, resultOk);
 	}
-	
-	@Test
-	public void testGetCombinations() {
-		Set<Integer> s = new HashSet<Integer>();
-		s.add(10);
-		s.add(20);
-		s.add(30);
-		FrequentItemSetsMining mining = new FrequentItemSetsMining();
-		Set<Set<Integer>> r = mining.getCombinations(s, 2);
-		assertEquals("Incorrect Combinations", r.size(), 3);
-	}
-	
-	@Test
-	public void testUpdateItemSetsFrequencyMap() {
-		FrequentItemSetsMining mining = new FrequentItemSetsMining();
-		mining.itemSetsFrequency.put(ImmutableSet.of(1001), 20);
-		mining.updateItemSetsFrequencyMap(ImmutableSet.of(1001));
-		
-		mining.updateItemSetsFrequencyMap(ImmutableSet.of(101));
-		
-		int r1 = mining.itemSetsFrequency.get(ImmutableSet.of(1001));
-		int r2 = mining.itemSetsFrequency.get(ImmutableSet.of(101));
-		
-		assertEquals("Error updating Frequency Map", r1, 21);
-		assertEquals("Error initializing Frequency Map Count", r2, 1);
-	}
-	
-	@Test
-	public void testPruneFrequencyMap() {
-		FrequentItemSetsMining mining = new FrequentItemSetsMining();
-		mining.itemSetsFrequency.put(ImmutableSet.of(1001), 2);
-		mining.itemSetsFrequency.put(ImmutableSet.of(1003), 3);
-		mining.SIGMA = 3;
-		
-		mining.pruneFrequencyMap();
-		assertEquals("Error Pruning Frequency Map", mining.itemSetsFrequency.size(), 1);
-	}
-	
-	@SuppressWarnings("unchecked")
-	@Test
-	public void testGuavaSets() {
-		Set<Integer> A = Set.of(10,20);
-		Set<Integer> B = Set.of(20,30);
-		Set<Integer> r = null;
-		//if (Sets.intersection(A, B).size() > 0) {
-			r = Sets.union(A,B);
-		//}
-		System.out.println(r.toString());
-		
-	}
-	*/
-	/*
-	@Test
-	public void testGetPossibleItemSets() {
-		FrequentItemSetsMiningOptimized mining = new FrequentItemSetsMiningOptimized();
-		Set<Set<Integer>> prevItemSets = new HashSet();
-		Set<Integer> a = Set.of(10);
-		Set<Integer> b = Set.of(20);
-		Set<Integer> c = Set.of(30);
-		prevItemSets.add(a);
-		prevItemSets.add(b);
-		prevItemSets.add(c);
-		Set<Set<Integer>> r = mining.getPossibleItemSets(prevItemSets, 2);
-		//for (Set<Integer> s : r) System.out.println(s.toString());
-		
-		Set<Set<Integer>> prevItemSets2 = new HashSet();
-		Set<Integer> d = Set.of(10,20);
-		Set<Integer> e = Set.of(20,30);
-		Set<Integer> f = Set.of(30,50);
-		prevItemSets2.add(d);
-		prevItemSets2.add(e);
-		prevItemSets2.add(f);
-		Set<Set<Integer>> r2 = mining.getPossibleItemSets(prevItemSets2, 3);
-		//for (Set<Integer> s2 : r2) System.out.println(s2.toString());
-		
-		Set<Set<Integer>> prevItemSets3 = new HashSet();
-		Set<Integer> x = Set.of(10,20,30);
-		Set<Integer> y = Set.of(20,30,60);
-		Set<Integer> z = Set.of(30,60,250);
-		prevItemSets3.add(x);
-		prevItemSets3.add(y);
-		prevItemSets3.add(z);
-		Set<Set<Integer>> r3 = mining.getPossibleItemSets(prevItemSets3, 4);
-		//for (Set<Integer> s3 : r3) System.out.println(s3.toString());
-		
-		TreeSet<Integer> t = new TreeSet<Integer>();
-		t.add(50);
-		t.add(40);
-		t.add(25);
-		
-		//System.out.println(t.toString());
-		
-		Set<Integer> X = Set.of(77,88,99);
-		Set<Integer> Y = Set.of(88,99,60);
-		X.addAll(Y);
-		System.out.println("Last " + X.toString());
-		
-	}
-	*/
 }
